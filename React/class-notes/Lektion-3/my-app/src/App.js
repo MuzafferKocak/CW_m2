4dae752c0a5acece789c66fd4a2a7aafa2ddd311
Mahir <mkojak75@gmail.com>
2024-03-21 20:39:07 +0100

    props

---
+++ React/class-notes/Lektion-3/my-app/src/App.js
@@ -0,0 +1,17 @@
+
+
+//? Component
+function App() {
+  //! JS Code
+  const deneme = "REACT"
+  return (
+
+    //! JSX (HTML ile JS in birlesmis hali)
+    <div className="App">
+      <h2 style={{color: "red"}}>Hosgeldin Ya Sehri React</h2>
+      <p className="par">{deneme}</p>
+    </div>
+  );
+}
+
+export default App;
