1311c5ee049e3b2467436452d5980856779e1ce0
Mahir <mkojak75@gmail.com>
2024-04-06 11:32:30 +0200

    router

---
+++ React/class-notes/Lektion-10/router-app/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+const Nav = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/instructors">Instructors</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+  
+  export default Nav
