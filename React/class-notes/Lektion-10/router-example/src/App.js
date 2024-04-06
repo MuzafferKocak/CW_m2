afae58d1aff80b623b40763c7fa1f8a0b7f460fb
Mahir <mkojak75@gmail.com>
2024-04-06 19:35:41 +0200

    useNavigate

--- React/class-notes/Lektion-10/router-example/src/App.js
+++ React/class-notes/Lektion-10/router-example/src/App.js
@@ -4,6 +4,7 @@ import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import People from "./pages/People"
 import Paths from "./pages/Paths"
+import PersonDetail from "./pages/PersonDetail"
 import NotFound from "./pages/NotFound"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/paths" element={<Paths />} />
         <Route path="*" element={<NotFound />} />
--- React/class-notes/Lektion-10/router-example/src/pages/Instructors.jsx
+++
@@ -1,7 +0,0 @@
-const Instructors = () => {
-    return (
-      <div>Instructors</div>
-    )
-  }
-  
-  export default Instructors
