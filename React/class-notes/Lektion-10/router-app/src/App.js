5aca12dd0a71a0352ceed084834e5226d4f66646
Mahir <mkojak75@gmail.com>
2024-04-06 12:37:43 +0200

    example

--- React/class-notes/Lektion-10/router-app/src/App.js
+++ React/class-notes/Lektion-10/router-app/src/App.js
@@ -4,17 +4,21 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructers" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- React/class-notes/Lektion-10/router-app/src/components/Nav.jsx
+++ React/class-notes/Lektion-10/router-app/src/components/Nav.jsx
@@ -1,19 +1,21 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Nav = () => {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/instructors">Instructors</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
-  
-  export default Nav
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/instructors">Instructors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
