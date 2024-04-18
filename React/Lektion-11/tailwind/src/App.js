c15dbf28954a83bb93792a0a3d32c7f9f908bcb3
Mahir <mkojak75@gmail.com>
2024-04-18 12:20:41 +0200

    tailwind

---
+++ React/Lektion-11/tailwind/src/App.js
@@ -0,0 +1,20 @@
+import HoverAndFocus from "./components/HoverAndFocus";
+import SpacingAndSizing from "./components/SpacingAndSizing";
+import Typografy from "./components/Typografy";
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
+
+    {/* <Typografy/> */}
+    {/* <SpacingAndSizing/> */}
+    <HoverAndFocus/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Lektion-11/tailwind/src/components/Typografy.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Typografy = () => {
+  return (
+    <div>
+    {/* //* Typografy */}
+    <h3 className='text-2xl'>Typografy</h3>
+    <p className='text-sm'>Typografy</p>
+    <p className='text-lg'>Typografy</p>
+    <p className='text-xl'>Typografy</p>
+    <p className='text-2xl'>Typografy</p>
+    <h2 className='text-3xl'>Typografy</h2>
+    <h1 className='text-4xl'>Typografy</h1>
+    <h1 className='text-[60px]'>Typografy</h1>
+    <h1 className='text-base/8'>Typografy</h1>
+    {/* Line clamp */}
+
+    <article>
+        <p className='line-clamp-1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quam atque quia placeat ipsum dolor consectetur, aperiam asperiores, in aliquid tenetur laudantium nihil, ad vitae? Consectetur, adipisci similique ipsum modi quo consequatur odit sequi laudantium porro laboriosam velit deleniti autem libero repellendus, ex facere. Eligendi aperiam maxime sint voluptate repellendus!</p>
+
+
+        
+    </article>
+    <p className='text-center'>Merhaba</p>
+        <p className='underline'>Merhaba</p>
+        <p className='line-through'>Merhaba</p>
+        <p className='uppercase'>Merhaba</p>
+        <p className='lowercase'>Merhaba</p>
+        <p className='text-red-400'>Merhaba</p>
+        <p className='text-red-900'>Merhaba</p>
+        <p className='text-green-900'>Merhaba</p>
+        <p className='text-[#1E0342] font-bold'>Merhaba</p>
+        <p className='text-[rgb(154,200,205)] font-bold'>Merhaba</p>
+        <p className='bg-slate-500 font-bold'>Merhaba</p>
+        <p className='bg-[#E1F7F5] font-bold'>Merhaba</p>
+    </div>
+  )
+}
+
+export default Typografy
