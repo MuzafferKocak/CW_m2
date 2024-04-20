71c2f716c2d1f0ea5f6ffc79234097b8e019cccb
Mahir <mkojak75@gmail.com>
2024-04-20 09:56:59 +0200

    update

--- React/class-notes/Lektion-11/tailwind/src/App.js
+++ React/class-notes/Lektion-11/tailwind/src/App.js
@@ -1,5 +1,6 @@
 import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverAndFocus from "./components/HoverAndFocus";
+import Navbar from "./components/Navbar";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typografy from "./components/Typografy";
 
@@ -8,12 +9,13 @@ import Typografy from "./components/Typografy";
 function App() {
   return (
     <div className="App">
+    <Navbar/>
     <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
 
     {/* <Typografy/> */}
     {/* <SpacingAndSizing/> */}
     {/* <HoverAndFocus/> */}
-    <FlexAndResponsive/>
+    {/* <FlexAndResponsive/> */}
       
     </div>
   );
