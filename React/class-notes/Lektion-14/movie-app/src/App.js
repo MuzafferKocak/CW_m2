95182c3fb4b81e95134947b3ab6ef98381cf5d63
Mahir <mkojak75@gmail.com>
2024-04-25 21:12:45 +0200

    main

--- React/class-notes/Lektion-14/movie-app/src/App.js
+++ React/class-notes/Lektion-14/movie-app/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import AppRouter from "./router/AppRouter";
+import AuthProvider from "./context/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import MovieProvider from "./context/MovieProvider";
 
-import Register from "./pages/Register";
-
-
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Register />
+    <div className="dark:bg-gray-dark-main min-h-screen">
+      <AuthProvider>
+        <MovieProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieProvider>
+      </AuthProvider>
     </div>
   );
-}
+};
 
 export default App;
