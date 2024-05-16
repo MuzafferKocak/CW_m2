62a491d048b03de87db3305590d50241944e3803
Mahir <mkojak75@gmail.com>
2024-05-16 19:22:26 +0200

    sales

--- React/class-notes/Lektion-18/stock-app/src/components/DataFetchMessages.jsx
+++ React/class-notes/Lektion-18/stock-app/src/components/DataFetchMessages.jsx
@@ -1,30 +1,30 @@
-import Alert from "@mui/material/Alert"
-import Skeleton from "@mui/material/Skeleton"
-import Stack from "@mui/material/Stack"
+import Alert from "@mui/material/Alert";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 
 export const ErrorMessage = () => {
   return (
     <Alert sx={{ mt: 3 }} severity="error">
       Veriler çekilemedi
     </Alert>
-  )
-}
+  );
+};
 
 export const NoDataMessage = () => {
   return (
     <Alert sx={{ mt: 3 }} severity="warning">
-      Gösterilecek veri bulunamadı
+      Gösterilecek veri bulunamadi
     </Alert>
-  )
-}
+  );
+};
 
 export const CardSkeleton = ({ children }) => {
   return (
     <Stack spacing={1} sx={{ mt: 3 }}>
       <Skeleton variant="rectangular">{children}</Skeleton>
     </Stack>
-  )
-}
+  );
+};
 
 const TableSkeleton = () => {
   return (
@@ -35,7 +35,7 @@ const TableSkeleton = () => {
       <Skeleton variant="rectangular" width="100%" height={50} />
       <Skeleton variant="rectangular" width="100%" height={30} />
     </Stack>
-  )
-}
+  );
+};
 
-export default TableSkeleton
+export default TableSkeleton;
--- React/class-notes/Lektion-18/stock-app/src/pages/Sales.jsx
+++ React/class-notes/Lektion-18/stock-app/src/pages/Sales.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import useStockRequest from "../services/useStockRequest"
+import { Button, Container } from "@mui/material"
+import SalesModal from "../components/SalesModal"
+import SalesTable from "../components/SalesTable"
+import TableSkeleton, {
+  // ErrorMessage,
+  NoDataMessage,
+} from "../components/DataFetchMessages"
+import { useSelector } from "react-redux"
 
 const Sales = () => {
+  const { getStock } = useStockRequest()
+  const { sales, loading } = useSelector((state) => state.stock)
+
+  const [open, setOpen] = useState(false)
+
+  const initialState = { brandId: "", productId: "", quantity: "", price: "" }
+  const [info, setInfo] = useState(initialState)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setInfo(initialState)
+  }
+
+  useEffect(() => {
+    getStock("products")
+    getStock("sales")
+    getStock("brands")
+  }, []) // eslint-disable-line
+
   return (
-    <div>Sales</div>
+    <Container maxWidth="xl">
+      <Button variant="contained" onClick={handleOpen}>
+        New Sale
+      </Button>
+
+      {loading && <TableSkeleton />}
+      {!loading && !sales?.length && <NoDataMessage />}
+
+      {!loading && sales?.length > 0 && (
+        <SalesTable setInfo={setInfo} handleOpen={handleOpen} />
+      )}
+
+      <SalesModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+    </Container>
   )
 }
 
