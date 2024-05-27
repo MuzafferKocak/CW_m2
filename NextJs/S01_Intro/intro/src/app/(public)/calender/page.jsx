c44ec994b93732227224b699f58d63286d385277
Mahir <mkojak75@gmail.com>
2024-05-27 22:10:30 +0200

    update

---
+++ NextJs/S01_Intro/intro/src/app/(public)/calender/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calender = () => {
+  return <div className="text-2xl text-center">Calender</div>;
+};
+
+export default Calender;
---
+++ NextJs/S01_Intro/intro/src/app/(public)/contacts/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contacts = () => {
+  return <div className="text-2xl text-center">Contacts</div>;
+};
+
+export default Contacts;
